f8c46d7634aa01c5daf0415d6dec1dd54acea55a
cungtrang0978 <cungtrang0978@gmail.com>
2022-08-07 16:55:57 +0700

    upload new source

---
+++ BaiTapLon/Controllers/PromotionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTapLon.Controllers
+{
+    public class PromotionController : Controller
+    {
+        // GET: Promotion
+        public ActionResult Index()
+        {
+            ViewBag.title = "Khuyến mãi";
+            return View();
+        }
+    }
+}
